ac038fd4168f5f89028383c786fd1f5e801827c9
*jimin* <*jimin*>
2025-03-17 16:06:58 +0900

    fix: 임포트 Navbar->navbar.jsx로 수정

--- frontend/src/main-page.jsx
+++ frontend/src/main-page.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "~/components/Navbar.jsx";
+import Navbar from "~/components/navbar.jsx";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
