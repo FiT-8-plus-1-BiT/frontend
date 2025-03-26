aabce7700cd8b36851ec0d8dc9946b04e15ec326
E0min <bbok4yo@gmail.com>
2025-03-26 22:21:31 +0900

    fix: config파일 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -23,20 +23,5 @@ export default defineConfig({
       "~": path.resolve(__dirname, "src"),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://fit-conf.shop',
-        changeOrigin: true,
-        secure: false,
-        ws: true // WebSocket 프록시 활성화
-      },
-      '/ws': {  // WebSocket 경로 추가
-        target: 'https://fit-conf.shop',
-        changeOrigin: true,
-        secure: false,
-        ws: true
-      }
-    }
-  }
+  
 });
