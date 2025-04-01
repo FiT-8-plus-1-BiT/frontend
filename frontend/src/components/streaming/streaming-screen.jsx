de78b3aa4c2b12cf8625c6b63dae3e423023eaad
E0min <bbok4yo@gmail.com>
2025-04-02 01:51:58 +0900

    hotfix

--- frontend/src/components/streaming/streaming-screen.jsx
+++ frontend/src/components/streaming/streaming-screen.jsx
@@ -1,36 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import YouTube from 'react-youtube';
-import axios from 'axios';
 
 const StreamingScreen = () => {
-    const [videoId, setVideoId] = useState(null);
-
-    useEffect(() => {
-        const fetchRandomLiveStream = async () => {
-            try {
-                const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
-                    params: {
-                        part: 'id,snippet',
-                        eventType: 'live',
-                        type: 'video',
-                        maxResults: 10,
-                        key: 'AIzaSyCRyXnQeElOoYUdv_Ehs6qrnPBwJDoIVAE',
-                    },
-                });
-
-                const items = response.data.items;
-                if (items && items.length > 0) {
-                    const randomIndex = Math.floor(Math.random() * items.length);
-                    const randomVideoId = items[randomIndex]?.id?.videoId;
-                    setVideoId(randomVideoId);
-                }
-            } catch (error) {
-                console.error('Error fetching live stream:', error);
-            }
-        };
-
-        fetchRandomLiveStream();
-    }, []);
+    const videoId = 'FJfwehhzIhw'; // 🔹 특정 라이브 스트림 Video ID 직접 설정
 
     const opts = {
         width: '100%',
@@ -44,13 +16,9 @@ const StreamingScreen = () => {
 
     return (
         <div className="w-full bg-gray-200 md:h-[42.6vw] h-[79.8vw]">
-            {videoId ? (
-                <div className="w-full h-full">
-                    <YouTube videoId={videoId} opts={opts} className="w-full h-full" />
-                </div>
-            ) : (
-                <p>Loading random live stream...</p>
-            )}
+            <div className="w-full h-full">
+                <YouTube videoId={videoId} opts={opts} className="w-full h-full" />
+            </div>
         </div>
     );
 };
